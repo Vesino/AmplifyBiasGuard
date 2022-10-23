4e27544dbd09826992b84f60f720b01ea026e9b7
daksen <carlos.tavcon@gmail.com>
2022-10-23 09:59:15 -0500

    code changes

--- amplify/backend/function/documents/src/analyzer/runner.py
+++ amplify/backend/function/documents/src/analyzer/runner.py
@@ -22,33 +22,37 @@ class DocumentAnalyzer:
     def run(self):
         s3 = boto3.client("s3")
         data = s3.get_object(Bucket="amplifybiasguard-storage35827-dev", Key=self.key)
-        contents = data["Body"].read()
+        contents = data["Body"].read().decode('utf-8')
 
         classification_response = self.comprehend.classify_document(
             Text=contents,
             EndpointArn=os.environ['COMPREHEND_MODEL_ARN']
         )
 
-        classification_response = {
-            "Classes": [],
-            "Labels": [
-                {"Name": "Male", "Score": 0.4},
-                {"Name": "Female", "Score": 0.25},
-                {"Name": "Neutral", "Score": 0.35},
-            ],
-        }
+        # classification_response = {
+        #     "Classes": [],
+        #     "Labels": [
+        #         {"Name": "Male", "Score": 0.4},
+        #         {"Name": "Female", "Score": 0.25},
+        #         {"Name": "Neutral", "Score": 0.35},
+        #     ],
+        # }
 
         scores = {
-            l["Name"]: l["Score"] 
-            for l in classification_response["Labels"]
+            l["Name"].lower(): l["Score"] 
+            for l in classification_response["Classes"]
         }
 
         item = {
             "url": self.key,
-            "male": Decimal(str(scores["Male"])),
-            "female": Decimal(str(scores["Female"])),
-            "neutral": Decimal(str(scores["Neutral"])),
+            "male": Decimal(str(scores.get("male", 0))),
+            "female": Decimal(str(scores.get("female", 0))),
+            "neutral": Decimal(str(scores.get("neutra", 0))),
         }
 
         self.store_url_cache(item=item)
-        return
+        
+        item.pop('url')
+        key, value = max(item.items(), key=lambda pair: pair[1])
+        
+        return {key: value}
